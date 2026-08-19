f80680bd0a89a55eef97ba27caee504f9e72fdae
normansyazwan96 <normansyazwan96@gmail.com>
2026-08-19 11:49:32 +0800

    Fix fetch call syntax in chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -35,7 +35,7 @@ async function sendMessage() {
     input.value = "";
 
     try {
-        const response = await fetch("http://127.0.0.1:8000/chat", {
+        const response = await fetch("http://127.0.0.1:8000/chat"), {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
